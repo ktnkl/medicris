b29e1bc317993233d50f2d267dec943a42187419
Alexey_Pankov <pankov.kek@gmail.com>
2024-07-14 21:07:21 +0300

    делаю параллакс

---
+++ parallax.js
@@ -0,0 +1,20 @@
+const page2 = document.getElementById("page-2")
+console.log(page2)
+const gradient = document.getElementById("gradient")
+const abilities = document.getElementById("abilities")
+const doctors = document.getElementById("doctors")
+const equip = document.getElementById("equip")
+const clinic = document.getElementById("clinic")
+
+window.addEventListener("scroll", () => {
+  console.log(scrollY)
+  if (Math.round(scrollY) > 617 ) {
+    page2.classList.add('page-2-active')
+  } else {
+    page2.classList.remove('page-2-active')
+  }
+
+  if (Math.round(scrollY) > 1787) {
+    
+  }
+})
--- swipers.js
+++ swipers.js
@@ -1,18 +1,8 @@
-// import Swiper JS
 import Swiper from './node_modules/swiper/swiper-bundle.min.mjs';
-// import Swiper styles
-
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'horizontal',
   loop: true,
   speed: 600,
   
 });
- const swiper2 = new Swiper(".swiper-container-feedback", {
-  direction: 'horizontal',
-  speed: 600,
-  nextEl: '.feedback__scroller__r',
-  prevEl: 'feedback__scroller__l'
- })
